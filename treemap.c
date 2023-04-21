0184972ce1df65cb37e184332de3b7f7525ae563
Replit user <>
2023-04-21 04:01:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -74,7 +74,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
   while ( current != NULL){
 
-    if( current->lower_than(current->pair->key, key) == 1){
+    if( tree->lower_than(current->pair->key, key) == 1){
       if( current->right == NULL) break;
       current = current->right;
     }
